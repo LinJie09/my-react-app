4d85c75cf8186eea024657045213b427ccecd563
LinJie09 <zxc664231@gmail.com>
2022-03-02 22:05:33 +0800

    2022/03/02

--- src/App.js
+++ src/App.js
@@ -1,25 +1,16 @@
 import logo from './logo.svg';
 import './App.css';
-
+import {useState} from 'react'
 function App() {
+  const [number,setNumber] = useState(0)
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className='App'>
+      <header className='App-header'>
+        <div>Counter : {number}</div>
+        <button onClick={()=>{setNumber(number+1)}}>加加</button>
+        <button onClick={()=>{setNumber(number-1)}}>減減</button>
       </header>
     </div>
   );
 }
-
 export default App;
