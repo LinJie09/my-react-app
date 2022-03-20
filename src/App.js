89bf8fa239ba57f7d2346aa49ebd1ecd8807d94d
LinJie09 <zxc664231@gmail.com>
2022-03-20 14:28:22 +0800

    2022/03/20

--- src/App.js
+++ src/App.js
@@ -1,16 +1,41 @@
 import logo from './logo.svg';
 import './App.css';
-import {useState} from 'react'
+import Slider from './Slider/Slider';
+import { useState } from 'react'
 function App() {
-  const [number,setNumber] = useState(0)
-  return (
-    <div className='App'>
-      <header className='App-header'>
-        <div>Counter : {number}</div>
-        <button onClick={()=>{setNumber(number+1)}}>加加</button>
-        <button onClick={()=>{setNumber(number-1)}}>減減</button>
-      </header>
-    </div>
+  return(
+    <>
+      <Slider></Slider>
+    </>
   );
+  // const [number, setNumber] = useState(0)
+  // return (
+  //   <div className='container'>
+  //     <body>
+  //       <h1>Counter</h1>
+  //       <span className={`value ${number > 0 ? 'text-green' : 'text-red'} ${number === 0 && 'text-black'}`} id='value'>{number}</span>
+  //       <div className='btn-container'>
+
+  //         <button className='btn increase'
+  //           onClick={() => {
+  //             setNumber(number - 1)
+  //           }}>Dncrease
+  //         </button>
+
+  //         <button className='btn reset'
+  //           onClick={() => {
+  //             setNumber(0)
+  //           }}>Reset
+  //         </button>
+
+  //         <button className='btn increase'
+  //           onClick={() => {
+  //             setNumber(number + 1)
+  //           }}>Increase
+  //         </button>
+  //       </div>
+  //     </body>
+  //   </div>
+  // );
 }
 export default App;
